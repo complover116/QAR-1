fbc5e440630c8da55a3803c215efae99f8a54819
complover116 <complover116@PootisBook>
2015-08-08 23:41:14 +0300

    Finally fixed the CI script I am so MLG

--- CI_Deployment_Script.sh
+++ CI_Deployment_Script.sh
@@ -1,4 +1,7 @@
-mkdir ./QAR-1/build-$TRAVIS_BUILD_NUMBER/
-gzip ./build/distributions/QAR-1/QAR-1.tar
-cp ./build/distributions/QAR-1/QAR-1.tar.gz ./QAR-1/build-$TRAVIS_BUILD_NUMBER/
+PROJNAME=QAR-1
+
+mkdir $PROJNAME
+mkdir $PROJNAME/build-$TRAVIS_BUILD_NUMBER/
+gzip build/distributions/$PROJNAME.tar
+cp build/distributions/$PROJNAME.tar.gz $PROJNAME/build-$TRAVIS_BUILD_NUMBER/
 
