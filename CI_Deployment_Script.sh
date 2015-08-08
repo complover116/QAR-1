ff77bd67824cd88c28d32d7ab78d8a484f4083e3
complover116 <complover116@PootisBook>
2015-08-09 00:57:18 +0300

    Now the script deletes everything but the src and QAR-1 dirs

--- CI_Deployment_Script.sh
+++ CI_Deployment_Script.sh
@@ -2,6 +2,11 @@ PROJNAME=QAR-1
 
 mkdir $PROJNAME
 mkdir $PROJNAME/build-$TRAVIS_BUILD_NUMBER/
+#GZ the tarball and copy it to the output folder
 gzip build/distributions/$PROJNAME.tar
 cp build/distributions/$PROJNAME.tar.gz $PROJNAME/build-$TRAVIS_BUILD_NUMBER/
+#Remove everything so travis does not get confused
+rm -f *
+rm -rf build
+rm -rf gradle
 
