5f2e31d6ab6f243de86ac4b41d6b45411f8bd478
complover116 <complover116@PootisBook>
2015-08-08 23:32:39 +0300

    Fixed deployment script

--- CI_Deployment_Script.sh
+++ CI_Deployment_Script.sh
@@ -1,4 +1,4 @@
 mkdir ./QAR-1/build-$TRAVIS_BUILD_NUMBER/
-cp ./build/distributions/QAR-1/* ./QAR-1/build-$TRAVIS_BUILD_NUMBER/
-
+gzip ./build/distributions/QAR-1/QAR-1.tar
+cp ./build/distributions/QAR-1/QAR-1.tar.gz ./QAR-1/build-$TRAVIS_BUILD_NUMBER/
 
