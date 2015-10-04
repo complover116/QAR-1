380944307b964b194f9d8f0b6cf4812fabc4b67a
complover116 <complover116@gmail.com>
2015-10-04 16:18:08 +0300

    Increased ticktime theshold from 0.2 to 0.25

--- core/src/com/complover116/q1r/Projectile.java
+++ core/src/com/complover116/q1r/Projectile.java
@@ -22,7 +22,7 @@ public class Projectile extends Entity {
 		if(time>0.02) {
 			time = 0;
 			for(int i = 0; i < 3; i ++)
-				if(Settings.ticktime<0.2)
+				if(Settings.ticktime<0.25)
 				GameWorld.ents.add(new Particle(x, y, 4, 4, this.color, (float)Math.random()*200-100, (float)Math.random()*200-100, false, true));
 if(Settings.ticktime<0.3)			
 GameWorld.ents.add(new Particle(x, y, 8, 16, this.color, 0, 0, false, false));
